47bda067d13c79f97426d4b1b303d9583e703b33
csvaishakh <vaishakhsubramonian@gmail.com>
2025-12-15 04:06:42 +0000

    fix [auth] : updated middleware logic

--- src/lib/auth/auth-middleware.ts
+++ src/lib/auth/auth-middleware.ts
@@ -4,11 +4,12 @@ import { createMiddleware } from "hono/factory";
 export const requireAuth = createMiddleware(async (c, next) => {
     const sessionData = await auth.api.getSession({ headers: c.req.raw.headers });
     
-    if (sessionData) {
-        c.set("Session", sessionData);
-        await next();
-        return
-    }else{
+    if (!sessionData) {
          return c.json({error: "Unauthorized"}, 401)
     }
+    
+    c.set("session", sessionData);
+    c.set("user", sessionData.user);
+
+    await next();
 })
