2e225e9ab2b768c1b1dc1cf7da0eef4578170efe
csvaishakh <vaishakhsubramonian@gmail.com>
2026-03-27 10:36:36 +0530

    fix(aut) : set new base pathfor authroute

--- packages/auth/auth-client.ts
+++ packages/auth/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_WEB_APP_URL
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+    basePath: "/auth"
 });
 
 export const { getSession, useSession } = authClient;
