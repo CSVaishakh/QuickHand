68b846ffadd5bd7720d2564a461b5b39e951bd70
csvaishakh <vaishakhsubramonian@gmail.com>
2026-05-18 21:54:04 +0530

    feat(types): add handyman and customer type definitions

--- apps/web/lib/types/types.ts
+++ apps/web/lib/types/types.ts
@@ -2,7 +2,7 @@ export type handyman = {
     name: string,
     email: string,
     password: string,
-    category: "plumber" | "electrician" | "carpenter" | "mason" | "mechanic" | "havc_technician" | "landscaper" | "deep_cleaner"
+    category: "plumber" | "electrician" | "carpenter" | "mason" | "mechanic" | "hvac_technician" | "landscaper" | "deep_cleaner"
 }
 
 export type customer = {
