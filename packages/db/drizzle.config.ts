2beaddb41050a82e36bbff979551062673f07411
csvaishakh <vaishakhsubramonian@gmail.com>
2026-05-20 02:04:38 +0530

    fix(db): conenction erro solved by adding specic env for packages/db

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,16 +1,11 @@
+import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-    throw new Error("DATABASE_URL is not defined");
-}
-
 export default defineConfig({
-    schema: './schema.ts',
-    out: './drizzle',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: databaseUrl,
-    },
+  schema: "./schemas/*.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
 });
