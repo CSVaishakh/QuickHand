a6b190c1826d5505896607fe2d1761735a650862
csvaishakh <vaishakhsubramonian@gmail.com>
2026-05-20 11:01:15 +0530

    feat(auth): implement sign-up forms for customer and handyman with routing and error handling

---
+++ apps/web/app/auth/sign-up/_components/header.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+export default function SignUpHeader () {
+    return(
+        <section className="flex justify-center bg-white px-5 py-5 rounded-xl">
+            <div className="flex px-5">
+                <div>
+                    <span className="text-3xl font-bold">
+                        <span className="text-blue-400">Quick</span>
+                        <span className="text-black">Hand</span>
+                    </span>
+                </div>  
+            </div>
+        </section>
+    )
+}
