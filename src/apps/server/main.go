cbba5cbfab2df6828805055affdd170d5e06b40f
C S Vaishakh <vaishakhsubramonian@gmail.com>
2026-05-31 19:01:15 +0530

    Initialize project structure with essential files and server setup

---
+++ src/apps/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	//loading enviornment variables
+	err:= godotenv.Load(".env.local");
+	if err != nil {
+		log.Println("No env fil found")
+	}
+
+	//creating the fiber app instance
+	app:= fiber.New();
+	
+	//loading the post from env
+	port:= os.Getenv("PORT")
+
+	app.Get("/", func(c fiber.Ctx) error {
+		//logging server is working
+		return c.Res().SendString("Welcome to QuickHand");
+	});
+
+	log.Fatal(app.Listen(":" + port))
+} 
