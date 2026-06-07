ba9316dc426dfe5ece1766bf25bfed35638cfc80
C S Vaishakh <vaishakhsubramonian@gmail.com>
2026-06-07 21:55:57 +0530

    feat: initialize auth service and repository dependencies and  update environment variable comments

--- src/apps/server/main.go
+++ src/apps/server/main.go
@@ -4,32 +4,44 @@ import (
 	"log"
 	"os"
 
-	"github.com/CSVaishakh/QuickHand/src/packages/db"
+	DB "github.com/CSVaishakh/QuickHand/src/packages/db"
+	repositories "github.com/CSVaishakh/QuickHand/src/packages/db/repositories"
+	auth "github.com/CSVaishakh/QuickHand/src/packages/auth/src"
+
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	//loading enviornment variables
-	err:= godotenv.Load(".env.local");
+	//loading environment variables
+	err := godotenv.Load(".env.local")
 	if err != nil {
 		log.Println("No env fil found")
 	}
 
-	//starting the databse service
-	db.Init()
+	//establishing the db connection
+	db := DB.Init()
+	
+	//creating the repository instances
+	handymenRepo := repositories.NewHandymenRepository(db)
+	clientRepo := repositories.NewClientRepository(db)
+	sessionRepo := repositories.NewSessionRepository(db)
+
+	// creating the auth serivce instance
+	jwtSerivce := auth.NewJWTService(os.Getenv("AUTH_SECRET"))
+	authService := auth.NewAuthService(handymenRepo, clientRepo, sessionRepo, jwtSerivce,db)
 
 	//creating the fiber app instance
-	app:= fiber.New();
-	
+	app := fiber.New()
+
 	//loading the post from env
-	port:= os.Getenv("PORT")
+	port := os.Getenv("PORT")
 
 	app.Get("/", func(c fiber.Ctx) error {
 		//logging server is working
-		return c.Res().SendString("Welcome to QuickHand");
-	});
+		return c.Res().SendString("Welcome to QuickHand")
+	})
 
 	log.Fatal(app.Listen(":" + port))
-} 
+}
