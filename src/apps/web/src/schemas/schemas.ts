7efaf078f060ded85878baa853f0f2d1a71fcacf
vaishakh <vaishakhsubramonian@gmail.com>
2026-06-28 17:26:29 +0000

    feat(web): - configured dependancies- react-hook-form, zod - added request types and associated zod schemas for data validation - configured aliases to auth sdk in the auth package, and components dir - created auth-client instance
    -

---
+++ src/apps/web/src/schemas/schemas.ts
@@ -0,0 +1,89 @@
+import { z } from "zod";
+import { UserRole, HandymanType } from "#/types/types";
+
+const UserRoleSchema = z.enum([
+   UserRole.HANDYMAN,
+   UserRole.CUSTOMER
+])
+
+export const HandymanTypeSchema = z.enum([
+  HandymanType.PLUMBER,
+  HandymanType.ELECTRICIAN,
+  HandymanType.CARPENTER,
+  HandymanType.MASON,
+  HandymanType.MECHANIC,
+  HandymanType.HVAC_TECHNICIAN,
+  HandymanType.LANDSCAPER,
+  HandymanType.DEEP_CLEANER,
+])
+
+export const SignInReqSchema = z.object({
+   email:      z.email(),
+   password:   z.string().min(8),
+   role:       UserRoleSchema
+})
+
+export const HandymanSignUpReqSchema = z.object({
+   firstName:     z.string().min(1).max(25),
+   lastName:      z.string().min(1).max(25),
+   email:         z.email(),
+   password:      z.string().min(8),
+   type:         HandymanTypeSchema,
+})
+
+export const ClientSignUpReqSchema = z.object({
+   firstName:     z.string().min(1).max(25),
+   lastName:      z.string().min(1).max(25),
+   email:         z.email(),
+   password:      z.string().min(8),
+})
+
+export const  GetSessionReq = z.object({
+   token: z.string()
+})
+
+export const HandymanSignInResSchema = z.object({
+   userId:        z.string().min(1),
+   firstName:     z.string().min(1).max(25),
+   token: 		   z.string().min(1),
+   role:			   UserRoleSchema,
+   type:          HandymanTypeSchema,
+})
+
+export const ClientSignInResSchema = z.object({
+   userId:        z.string().min(1),
+   firstName:     z.string().min(1).max(25),
+   token: 		   z.string().min(1),
+	role:			   UserRoleSchema,
+})
+
+export const HandymanSignUpResSchema = z.object({
+   userId:        z.string().min(1),
+   firstName:     z.string().min(1).max(25),
+   token: 		   z.string().min(1),
+	role:			   UserRoleSchema,
+   type:          HandymanTypeSchema,
+})
+
+export const ClientSignUpResSchema = z.object({
+   userId:        z.string().min(1),
+   firstName:     z.string().min(1).max(25),
+   token: 		   z.string().min(1),
+   role:			   UserRoleSchema,
+})
+
+export const GetSessionResSchema = z.object({
+   sessionId:    z.uuid(),
+   revoked:      z.boolean(),
+   createdAt:    z.iso.datetime(),
+   userId:       z.uuid(),
+   firstName:    z.string(),
+   email:        z.email(),
+   role:         UserRoleSchema,
+   type:         HandymanTypeSchema.nullable(),
+})
+
+export const SignInResSchema = z.union([
+    ClientSignInResSchema,
+    HandymanSignInResSchema,
+]);
