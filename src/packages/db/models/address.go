a75a291aba524810388c695ae762fe34b234e475
vaishakh <vaishakhsubramonian@gmail.com>
2026-07-03 18:14:20 +0000

    style(models): remove redundant blank lines in db models -Clean up empty lines inside structural definitions for `Address`, `Session`, and `User` models.

--- src/packages/db/models/address.go
+++ src/packages/db/models/address.go
@@ -4,9 +4,7 @@ import "github.com/google/uuid"
 
 type Address struct {
 	AddressID 	uuid.UUID `gorm:"column:address_id;type:uuid;default:gen_random_uuid();primaryKey"`
-
 	UserID 		uuid.UUID `gorm:"column:user_id;type:uuid;not null"`
-
 	HouseNo 	string `gorm:"column:house_no;size:50;not null"`
 	Street  	string `gorm:"column:street;size:255;not null"`
 	City    	string `gorm:"column:city;size:100;not null"`
